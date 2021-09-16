56ce1a8fafde42fc10b48db74b5287ce34c416ab
Manpreet Kunnath <MVK059@users.noreply.github.com>
2021-09-16 09:55:05 +0530

    Add a possible fix for data binding error

--- mylibrary/src/main/java/com/mvk/mylibrary/LibraryActivity.kt
+++ mylibrary/src/main/java/com/mvk/mylibrary/LibraryActivity.kt
@@ -10,28 +10,28 @@ class LibraryActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityLibraryBinding
 
 	companion object {
-		private lateinit var logoutCallback: (Boolean) -> Unit
-
-		fun initLogoutCallback(logoutCallback : (Boolean) -> Unit) {
-			this.logoutCallback = logoutCallback
-		}
+//		private lateinit var logoutCallback: (Boolean) -> Unit
+//
+//		fun initLogoutCallback(logoutCallback : (Boolean) -> Unit) {
+//			this.logoutCallback = logoutCallback
+//		}
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = DataBindingUtil.setContentView(this, R.layout.activity_library)
-		setListeners()
+//		setListeners()
 	}
 
-	private fun setListeners() {
-		binding.libLogoutBtn.setOnClickListener {
-			logoutCallback(true)
-			finish()
-		}
-		binding.libLogoutFailBtn.setOnClickListener {
-			logoutCallback(false)
-			finish()
-		}
-	}
+//	private fun setListeners() {
+//		binding.libLogoutBtn.setOnClickListener {
+//			logoutCallback(true)
+//			finish()
+//		}
+//		binding.libLogoutFailBtn.setOnClickListener {
+//			logoutCallback(false)
+//			finish()
+//		}
+//	}
 
 }
